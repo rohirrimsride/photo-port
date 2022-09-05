939c1383b07876f2d803d4e8521922bd4449c361
Strider <dave.barnes23@gmail.com>
2022-09-05 17:02:47 -0500

    finished creating initial testing

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -21,13 +21,13 @@ describe('emoji is visible', () => {
     it('inserts emoji into the <h2>', () => {
         const { getByLabelText } = render(<Nav />);
         expect(getByLabelText('camera')).toHaveTextContent('📸');
-    })
-})
+    });
+});
 
 describe('links are visible', () => {
     it('inserts text into the links', () => {
         const { getByTestId } = render(<Nav />);
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
         expect(getByTestId('about')).toHaveTextContent('About me');
-    })
-})
+    });
+});
