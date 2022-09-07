c583184f5392e5981f6d044691548616444aafbc
Strider <dave.barnes23@gmail.com>
2022-09-07 17:46:43 -0500

    added conditional rendering to navbar elements

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ function App() {
 ]);
 const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+const [contactSelected, setContactSelected] = useState(false);
+
 
   return (
     <div>
@@ -35,11 +37,18 @@ const [currentCategory, setCurrentCategory] = useState(categories[0]);
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
